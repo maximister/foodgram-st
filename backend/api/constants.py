2ce60ca1c6597ef7d54967cb27d80c22b9f35e78
maximister <maximister44@gmail.com>
2025-06-02 02:34:28 +0300

    small changes

--- backend/api/constants.py
+++ backend/api/constants.py
@@ -1,6 +1,6 @@
 """Константы для пагинации."""
 from django.conf import settings
 
-PAGE_SIZE = getattr(settings, 'PAGE_SIZE', 10)
+PAGE_SIZE = getattr(settings, 'PAGE_SIZE', 6)
 PAGE_SIZE_PARAM = 'limit'
 MAX_PAGE_SIZE = 100
