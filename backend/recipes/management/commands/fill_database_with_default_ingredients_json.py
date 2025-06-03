ffcf7b84c1926942724866132f99cbf38f8b088d
maximister <maximister44@gmail.com>
2025-06-03 23:13:48 +0300

    update commands

--- backend/recipes/management/commands/fill_database_with_default_ingredients_json.py
+++ backend/recipes/management/commands/fill_database_with_default_ingredients_json.py
@@ -1,5 +1,5 @@
-"""Скрипт для наполнения базы ингредиентов дефолтными значениями."""
-import csv
+"""Скрипт для наполнения базы ингредиентов дефолтными значениями из JSON."""
+import json
 import os
 
 from django.conf import settings
@@ -10,49 +10,49 @@ from recipes.models import Ingredient
 
 class Command(BaseCommand):
     """Команда для наполнения базы ингредиентов дефолтными значениями \
-        из csv-файла."""
+        из json-файла."""
 
-    help = 'Загрузить список дефолтных ингредиентов из CSV файла'
+    help = 'Загрузить список дефолтных ингредиентов из JSON файла'
 
     def add_arguments(self, parser):
         """Парсинг аргументов из командной строки."""
         parser.add_argument(
             '--path',
             type=str,
-            help='Путь к CSV файлу c ингредиентами',
+            help='Путь к JSON файлу c ингредиентами',
             required=False
         )
 
     def handle(self, *args, **options):
-        """Перекладывание csv в бд."""
+        """Перекладывание json в бд."""
         try:
-            csv_path = options.get('path')
+            json_path = options.get('path')
 
-            if csv_path:
+            if json_path:
                 base_dir = os.path.dirname(os.path.dirname(
                     os.path.dirname(os.path.dirname(settings.BASE_DIR))))
-                csv_file = os.path.join(base_dir, 'foodgram-st', csv_path)
+                json_file = os.path.join(base_dir, 'foodgram-st', json_path)
             else:
                 data_dir = os.path.join(settings.BASE_DIR, '..', 'data')
-                csv_file = os.path.join(data_dir, 'ingredients.csv')
+                json_file = os.path.join(data_dir, 'ingredients.json')
 
-            with open(csv_file, 'r', encoding='utf-8') as file:
-                reader = csv.reader(file)
+            with open(json_file, 'r', encoding='utf-8') as file:
+                ingredients_data = json.load(file)
                 created = Ingredient.objects.bulk_create(
                     [
                         Ingredient(
-                            name=row[0].strip(),
-                            measurement_unit=row[1].strip()
+                            name=item['name'].strip(),
+                            measurement_unit=item['measurement_unit'].strip()
                         )
-                        for row in reader if len(row) == 2
+                        for item in ingredients_data
                     ],
                     ignore_conflicts=True
                 )
 
             self.stdout.write(
                 self.style.SUCCESS(
-                    f'Успешно загружено ингредиентов: {len(created)}.'
+                    f'Успешно загружено ингредиентов из JSON: {len(created)}.'
                 )
             )
         except Exception as e:
-            raise CommandError(f'Ошибка при чтении файла {csv_file}: {e}')
+            raise CommandError(f'Ошибка при чтении файла {json_file}: {e}')
