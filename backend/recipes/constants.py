8508e73f6046b5ee1289ab046b132782717e40ec
maximister <maximister44@gmail.com>
2025-06-05 01:51:58 +0300

    fix issues

--- backend/recipes/constants.py
+++ backend/recipes/constants.py
@@ -3,7 +3,7 @@
 MIN_COOKING_TIME = 1
 MIN_INGREDIENT_AMOUNT = 1
 MAX_NAME_LENGTH = 200
-MAX_COOKING_TIME_ERROR = 'Время приготовления не может быть больше 1 года'
+COOKING_TIME_ERROR = 'Время приготовления должно быть не менее 1 минуты'
 INGREDIENT_AMOUNT_ERROR = 'Количество ингредиента должно быть не менее 1'
 
 # Константы для админки
